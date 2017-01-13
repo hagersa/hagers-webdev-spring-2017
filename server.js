152828dca359144f18efe57d42c029549ef839e3
hagers <hager.s@husky.neu.edu>
2017-01-13 14:50:06 -0500

    changed server.js

--- server.js
+++ server.js
@@ -5,6 +5,16 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+var connectionString = 'mongodb://127.0.0.1:27017/test';
+
+if(process.env.MLAB_USERNAME) {
+	connectionString = process.env.MLAB_USERNAME + ":" +
+	process.env.MLAB_PASSWORD + "@" +
+	process.env.MLAB_HOST + ':' +
+	process.env.MLAB_PORT + '/' +
+	process.env.MLAB_APP_NAME;
+}
+
 // configure a public directory to host static content
 app.use(express.static(__dirname + '/public'));
 
