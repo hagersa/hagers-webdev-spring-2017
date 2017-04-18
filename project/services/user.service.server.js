0c6fd30d0c84912285c13b810381e7806b479ae4
hagers <hager.s@husky.neu.edu>
2017-04-18 19:12:07 -0400

    Fixed Google OAuth

--- project/services/user.service.server.js
+++ project/services/user.service.server.js
@@ -11,7 +11,7 @@ module.exports = function (app, OdhecatonUserModel) {
     var googleConfig = {
         clientID : process.env.GOOGLE_CLIENT_ID,
         clientSecret:  process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL : 'http://localhost:3000/auth/google/callback' //'https://hagers-webdev-spring-2017.herokuapp.com/auth/google/callback'
+        callbackURL : 'https://hagers-webdev-spring-2017.herokuapp.com/auth/google/callback' //'http://localhost:3000/auth/google/callback' 'https://hagers-webdev-spring-2017.herokuapp.com/auth/google/callback'
     };
 
     app.get("/api/user", findUser);
